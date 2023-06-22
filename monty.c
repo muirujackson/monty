0e74c42973fa1ec74d0c08af8ae9c0ebc6f60268
muirujackson <jakezweru@gmail.com>
2023-06-22 09:31:37 -0700

    added main file

---
+++ monty.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+
+int main(int argc, char **argv)
+{
+	stack_t *stack = NULL;
+
+	if (argc != 2)
+	{
+		fprintf(stdout, "USAGE: %s file\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	interpret_bytecode(argv[1]);
+	//free_stack(stack);
+	
+	return 0;
+}
